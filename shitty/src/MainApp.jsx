9079dd227a0491a7e183f8dc7d68bda4b98f9a32
seanrobertwright <seanrobertwright@gmail.com>
2020-10-23 15:45:57 -0400

    Added Side Navigation

--- shitty/src/MainApp.jsx
+++ shitty/src/MainApp.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+
 import '@ui5/webcomponents-icons/dist/icons/add.js';
-import { ShellBar, ShellBarItem, Avatar } from "@ui5/webcomponents-react";
+import '@ui5/webcomponents-icons/dist/icons/home.js';
+import '@ui5/webcomponents-icons/dist/icons/lab.js';
+import '@ui5/webcomponents-icons/dist/icons/employee.js';
+import '@ui5/webcomponents-icons/dist/icons/delete.js';
+import '@ui5/webcomponents-icons/dist/icons/customize.js';
+
+import { ShellBar, ShellBarItem, Avatar, SideNavigation, SideNavigationItem } from "@ui5/webcomponents-react";
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { Home } from "./Home";
 import {Detail } from "./Detail";
@@ -20,11 +27,19 @@ export function MainApp() {
                     <ShellBarItem icon="add" text="Add" />
             
             </ShellBar>
+            <SideNavigation>
+                <SideNavigationItem text="Home" icon="home"></SideNavigationItem>
+                <SideNavigationItem text="Chemical" icon="lab"></SideNavigationItem>
+                <SideNavigationItem text="Employee" icon="employee"></SideNavigationItem>
+                <SideNavigationItem text="Waste" icon="delete"></SideNavigationItem>
+                <SideNavigationItem text="Settings" icon="customize"></SideNavigationItem>
+            </SideNavigation>
             <Switch>
                 <Route path="/home" component={Home} />
                 <Route path="/detail" component={Detail} />
                 <Redirect from="/" to="/home" />
             </Switch>
+            
             </>
     );
 }
